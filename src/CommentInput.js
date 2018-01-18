b61739bd1cc3c89ccf1cbc12caeb71ab68b19b4f
marvin <mengfeng@chipwing.com>
2018-01-18 13:01:00 +0800

    fix(CommentInput): 完善commentInput组件

--- src/CommentInput.js
+++ src/CommentInput.js
@@ -7,16 +7,22 @@ import React, { Component } from 'react'
 class CommentInput extends Component {
   render () {
     return (
-      <div>
-        <div>
-          <span>用户名: </span>
-          <input type="text"/>
+      <div className="comment-input">
+        <div className="comment-field">
+          <span className='comment-field-name'>用户名: </span>
+          <div className='comment-field-input'>
+            <input type="text"/>
+          </div>
         </div>
-        <div>
-          <span>评论内容: </span>
-          <textarea></textarea>
+        <div className='comment-field'>
+          <span className='comment-field-name'>评论内容: </span>
+          <div className='comment-field-input'>
+            <textarea></textarea>
+          </div>
         </div>
-        <button>发布</button>
+        <div className='comment-field-button'>
+          <button>发布</button>
+        </div>
       </div>
     )
   }
