9dd2ad3d3f74f855fcad6c8840c9f8df6a495d94
marvin <mengfeng@chipwing.com>
2018-01-18 13:08:22 +0800

    fix(CommentInput): 输入框输入内容实现

--- src/CommentInput.js
+++ src/CommentInput.js
@@ -5,19 +5,39 @@
 import React, { Component } from 'react'
 
 class CommentInput extends Component {
+  constructor () {
+    super()
+    this.state = {
+      userName: '',
+      content: ''
+    }
+  }
+
+  handleUsernameChange (e) {
+    this.setState({
+      userName: e.target.value
+    })
+  }
+
+  handleContentChange (e) {
+    this.setState({
+      content: e.target.value
+    })
+  }
+
   render () {
     return (
       <div className="comment-input">
         <div className="comment-field">
           <span className='comment-field-name'>用户名: </span>
           <div className='comment-field-input'>
-            <input type="text"/>
+            <input type="text" value={this.state.userName} onChange={this.handleUsernameChange.bind(this)} />
           </div>
         </div>
         <div className='comment-field'>
           <span className='comment-field-name'>评论内容: </span>
           <div className='comment-field-input'>
-            <textarea></textarea>
+            <textarea value={this.state.content} onChange={this.handleContentChange.bind(this)} />
           </div>
         </div>
         <div className='comment-field-button'>
