3218386367098ed85fa069234f0356e29e533235
marvin <mengfeng@chipwing.com>
2018-01-19 16:58:26 +0800

    feat(CommentApp, wrapWithLoadData): 使用高阶组件重构评论组件

--- src/CommentApp.js
+++ src/CommentApp.js
@@ -5,32 +5,39 @@
 import React, { Component } from 'react'
 import CommentInput from './CommentInput'
 import CommentList from './CommentList'
+import PropTypes from 'prop-types'
+import wrapWithLoadData from "./wrapWithLoadData";
 
 class CommentApp extends Component {
-  constructor () {
-    super()
+  static propTypes = {
+    data: PropTypes.any,
+    saveData: PropTypes.func
+  }
+
+  constructor (props) {
+    super(props)
     this.state = {
-      comments: []
+      comments: props.data
     }
   }
 
-  componentWillMount () {
-    this._loadComments()
-  }
+  // componentWillMount () {
+  //   this._loadComments()
+  // }
 
   // 加载已存评论列表
-  _loadComments () {
-    let comments = localStorage.getItem('comments')
-    if (comments) {
-      comments = JSON.parse(comments)
-      this.setState({ comments })
-    }
-  }
+  // _loadComments () {
+  //   let comments = localStorage.getItem('comments')
+  //   if (comments) {
+  //     comments = JSON.parse(comments)
+  //     this.setState({ comments })
+  //   }
+  // }
 
   // 保存评论列表
-  _saveComments (comments) {
-    localStorage.setItem('comments', JSON.stringify(comments))
-  }
+  // _saveComments (comments) {
+  //   localStorage.setItem('comments', JSON.stringify(comments))
+  // }
 
 
   handleSubmitComment (comment) {
@@ -38,18 +45,23 @@ class CommentApp extends Component {
     if (!comment.userName) return alert('请输入用户名')
     if (!comment.content) return alert('请输入评论内容')
 
-    this.state.comments.push(comment)
-    this.setState({
-      comments: this.state.comments
-    })
-    this._saveComments(this.state.comments)
+    // this.state.comments.push(comment)
+    // this.setState({
+    //   comments: this.state.comments
+    // })
+    // this._saveComments(this.state.comments)
+    const comments = this.state.comments
+    comments.push(comment)
+    this.setState({ comments })
+    this.props.saveData(comments)
   }
 
   handleDeleteComment (index) {
     const comments = this.state.comments
     comments.splice(index, 1)
     this.setState({comments})
-    this._saveComments(comments)
+    // this._saveComments(comments)
+    this.props.saveData(comments)
   }
 
   render () {
@@ -62,4 +74,5 @@ class CommentApp extends Component {
   }
 }
 
+CommentApp = wrapWithLoadData(CommentApp, 'comments')
 export default CommentApp
