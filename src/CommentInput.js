ca750053a819cf79a10118cd375196beb89d6d10
marvin <mengfeng@chipwing.com>
2018-01-19 10:09:27 +0800

    feat(CommentInput): 自动聚焦评论框

--- src/CommentInput.js
+++ src/CommentInput.js
@@ -35,6 +35,10 @@ class CommentInput extends Component {
     })
   }
 
+  componentDidMount () {
+    this.textarea.focus()
+  }
+
   render () {
     return (
       <div className="comment-input">
@@ -47,7 +51,7 @@ class CommentInput extends Component {
         <div className='comment-field'>
           <span className='comment-field-name'>评论内容: </span>
           <div className='comment-field-input'>
-            <textarea value={this.state.content} onChange={this.handleContentChange.bind(this)} />
+            <textarea ref={textarea => this.textarea = textarea} value={this.state.content} onChange={this.handleContentChange.bind(this)} />
           </div>
         </div>
         <div className='comment-field-button'>
